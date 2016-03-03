ec477eec0b9fdb2144d7a1b841ced4ac2ccf6da4
Rudolf Cicko <alu0100824780@ull.edu.es>
2016-03-03 23:02:24 +0000

    Added constructor for Medida

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -5,11 +5,25 @@
 
   function Medida(valor,tipo)
   {
+    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
+        if(!tipo){
+          var expresion = XRegExp('(?<num>   [-+]?[0-9]+(\.[0-9]+)?[ ]*(?:e[+-]?[ ]*[0-9]+)?)[ ]*   # number       \n' +
+                                  '(?<tipo>    [fkcml])[ ]*                                       # inputTemp \n','x' + 'i');
+
+          var valor = XRegExp.exec(valor, expresion);
+
+          this.valor = valor.num;
+          this.tipo = valor.tipo;
+        }
+        /* ademas de new Medida(45.2, "Km") */
+        else{
+          this.valor = valor;
+          this.tipo = tipo;
+        }
 
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
   }
-
 
 
   function Temperatura(valor,tipo)
@@ -43,9 +57,9 @@
 
 
       if (valor) {
-       var inputTemp = new Medida(valor.input);
+       var inputTemp = new Medida(valor.num, valor.input);
 
-       var tipo   = valor[5].toLowerCase();
+       var tipo   = valor.output;
 
        //numero = parseFloat(numero);
        console.log("Valor: " + valor.input);
