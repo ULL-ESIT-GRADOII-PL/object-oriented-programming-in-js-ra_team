c83b8dffc56a7726177ee8ba21e91fcd8e1ef659
Rudolf Cicko <alu0100824780@ull.edu.es>
2016-03-03 22:37:16 +0000

    Added XRegExp expression

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -1,12 +1,16 @@
 
+
 (function(exports) {
   "use strict";
 
   function Medida(valor,tipo)
   {
+
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
   }
+
+
 
   function Temperatura(valor,tipo)
   {
@@ -25,36 +29,41 @@
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
 
-  exports.convertir = function() {
-  //  var XRegExp = require('xregexp');
-
-    var valor     = document.getElementById('convert').value,
+  exports.convert = function() {
+        var valor     = document.getElementById('convert').value,
         elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
-    valor     = valor.match(regexp);
+            /* Extienda la RegeExp a la especificación. use una XRegExp */
 
-    if (valor) {
-      var numero = valor[1],
-          tipo   = valor[2].toLowerCase();
+        expresion = XRegExp('(?<num>   [-+]?[0-9]+(\.[0-9]+)?[ ]*(?:e[+-]?[ ]*[0-9]+)?)[ ]*   # number       \n' +
+                                '(?<input>    [fkcmlFKCML])[ ]*                                       # inputTemp \n' +
+                                '(?<to>       (?:to)?)[ ]*                                        # to           \n' +
+                                '(?<output>    [fkcmlFKCML])[ ]*                                       # outputTemp','x');
 
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
+        valor = XRegExp.exec(valor, expresion);
 
-      switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          break;
 
-        default:
+      if (valor) {
+       var inputTemp = new Medida(valor.input);
+
+       var tipo   = valor[5].toLowerCase();
+
+       //numero = parseFloat(numero);
+       console.log("Valor: " + valor.input);
+
+       switch (tipo) {
+         case 'c':
+           var celsius = new Celsius(numero);
+           elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
+           break;
+         case 'f':
+           var farenheit = new Farenheit(numero);
+           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
+           break;
+
+         default:
           /* rellene este código */
-      }
-    }
+       }
+     }
     else
       elemento.innerHTML = "";
   };
